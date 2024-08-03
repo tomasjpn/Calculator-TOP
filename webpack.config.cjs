66dccf5f515f4ee1123bff8ff309d0a8fc1fe514
tomasjpn <tomasphammc@gmail.com>
2024-08-03 13:42:19 +0200

    Dateienstruktur des Projekts, src: index.html, index.js ; webpack.config.cjs: Konfiguration ; package.json

---
+++ webpack.config.cjs
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  mode: 'development',
+};
