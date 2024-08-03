83f86c97d42591fa1e782537c04a5652b6a77480
tomasjpn <tomasphammc@gmail.com>
2024-08-03 17:29:41 +0200

    index.hmtl: Struktur vom Projekt, styles.css: Design vom Taschenrechner

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -17,11 +17,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader'], // Process CSS files
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource', // Process image files
       },
     ],
   },
